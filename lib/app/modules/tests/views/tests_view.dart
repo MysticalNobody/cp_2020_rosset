e6121c96f7c83a6cc536dbdcff6f5b76164f7d46
Mikhail Kolchanov <01gicha01@gmail.com>
2020-11-14 01:04:35 +0300

    add layout to tests

--- lib/app/modules/tests/views/tests_view.dart
+++ lib/app/modules/tests/views/tests_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rosset_client/app/modules/tests/controllers/tests_controller.dart';
+import 'package:rosset_client/app/modules/tests/widgets/option.dart';
 import 'package:rosset_client/theme/app_colors.dart';
+import 'package:rosset_client/theme/app_text_styles.dart';
 import 'package:rosset_client/utils/unfocus_ext.dart';
 
 class TestsView extends GetView<TestsController> {
@@ -10,7 +12,78 @@ class TestsView extends GetView<TestsController> {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
-        children: [],
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Анатолич',
+                    style: AppTextStyles.subtitle.copyWith(
+                      color: AppColors.secondary,
+                    ),
+                  ),
+                  Text(
+                    'Тест на проверку освоенного материала',
+                    style: AppTextStyles.subtitle.copyWith(
+                      color: AppColors.secondary,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('Закончить тест'),
+                    onPressed: controller.exitTest,
+                  ),
+                ],
+              ),
+              SizedBox(height: Get.height * .2),
+              Column(
+                children: [
+                  Text(
+                    'Вопрос 1 блаопопшоук пзо укзсщка',
+                    style: AppTextStyles.headLine3,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Вопрос 1 из 20',
+                    style: AppTextStyles.secondary,
+                  ),
+                  SizedBox(height: 30),
+                  OptionButton(
+                    onTap: () {},
+                    text: 'Lorem Ipsum Dolores',
+                    type: OptionType.normal,
+                  ),
+                  OptionButton(
+                    onTap: () {},
+                    text: 'Lorem Ipsum Dolores',
+                    type: OptionType.error,
+                  ),
+                  OptionButton(
+                    onTap: () {},
+                    text: 'Lorem Ipsum Dolores',
+                    type: OptionType.right,
+                  ),
+                  OptionButton(
+                    onTap: () {},
+                    text: 'Lorem Ipsum Dolores',
+                    type: OptionType.normal,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: Get.height * .1),
+            alignment: Alignment.bottomCenter,
+            child: RaisedButton(
+              child: Text('Следующий вопрос'),
+              onPressed: controller.exitTest,
+            ),
+          ),
+        ],
       ),
     ).unfocusOnTap();
   }
