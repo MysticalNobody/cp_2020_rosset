b848263d8bd8f2fc9dd4d84f7b326be12b52456c
Kit <kit@itis.team>
2020-11-14 02:46:46 +0300

    fix long tap on web

--- lib/app/modules/home/widgets/instrument_card.dart
+++ lib/app/modules/home/widgets/instrument_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:rosset_client/app/data/model/device.dart';
 import 'package:rosset_client/theme/app_colors.dart';
@@ -11,69 +12,80 @@ class InstrumentCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return LongPressDraggable<DeviceModel>(
-          data: model,
-          feedback: Container(
-            width: constraints.maxWidth,
-            height: 120,
-            color: Colors.blueGrey.withOpacity(0.3),
+        var child = Container(
+          height: 120,
+          margin: EdgeInsets.only(bottom: 24),
+          decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.circular(3),
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.grey.withOpacity(.15),
+                blurRadius: 3,
+                spreadRadius: 1,
+              ),
+            ],
           ),
-          maxSimultaneousDrags: 1,
-          hapticFeedbackOnStart: true,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(bottom: 24),
-            decoration: BoxDecoration(
-              color: AppColors.white,
-              borderRadius: BorderRadius.circular(3),
-              boxShadow: [
-                BoxShadow(
-                  color: AppColors.grey.withOpacity(.15),
-                  blurRadius: 3,
-                  spreadRadius: 1,
-                ),
-              ],
-            ),
-            child: TextButton(
-              onPressed: () {},
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Row(
-                      children: [
-                        Image.network(
-                          'https://relematika.ru/upload/resize_cache/iblock/b3f/282_200_1/%D0%A2%D0%9E%D0%A0200_03_sm.jpg',
-                          height: 60,
-                          fit: BoxFit.contain,
+          child: TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Image.network(
+                        'https://relematika.ru/upload/resize_cache/iblock/b3f/282_200_1/%D0%A2%D0%9E%D0%A0200_03_sm.jpg',
+                        height: 60,
+                        fit: BoxFit.contain,
+                      ),
+                      SizedBox(width: 12),
+                      Flexible(
+                        child: Text(
+                          'Терминал релейной защиты ',
+                          style: AppTextStyles.subtitle,
                         ),
-                        SizedBox(width: 12),
-                        Flexible(
-                          child: Text(
-                            'Терминал релейной защиты ',
-                            style: AppTextStyles.subtitle,
-                          ),
-                        )
-                      ],
-                    ),
-                    // SizedBox(height: 6),
-                    // ClipRRect(
-                    //   borderRadius: BorderRadius.circular(6),
-                    //   child: TextButton(
-                    //     onPressed: () {},
-                    //     child: Text(
-                    //       'Подробнее',
-                    //       style: AppTextStyles.subtitle,
-                    //     ),
-                    //   ),
-                    // )
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                  // SizedBox(height: 6),
+                  // ClipRRect(
+                  //   borderRadius: BorderRadius.circular(6),
+                  //   child: TextButton(
+                  //     onPressed: () {},
+                  //     child: Text(
+                  //       'Подробнее',
+                  //       style: AppTextStyles.subtitle,
+                  //     ),
+                  //   ),
+                  // )
+                ],
               ),
             ),
           ),
         );
+        if (kIsWeb)
+          return Draggable<DeviceModel>(
+              data: model,
+              feedback: Container(
+                width: constraints.maxWidth,
+                height: 120,
+                color: Colors.blueGrey.withOpacity(0.3),
+              ),
+              maxSimultaneousDrags: 1,
+              child: child);
+        else
+          return LongPressDraggable<DeviceModel>(
+              data: model,
+              feedback: Container(
+                width: constraints.maxWidth,
+                height: 120,
+                color: Colors.blueGrey.withOpacity(0.3),
+              ),
+              maxSimultaneousDrags: 1,
+              hapticFeedbackOnStart: true,
+              child: child);
       },
     );
   }
