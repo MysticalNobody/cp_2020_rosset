ce28d9413b2ed07640f19445d233d1fe03162300
Mikhail Kolchanov <01gicha01@gmail.com>
2020-11-14 02:33:37 +0300

    add test logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'app/api/api.dart';
 import 'app/routes/app_pages.dart';
 import 'theme/app_theme.dart';
 
 void main() {
+  AppApi.init();
   runApp(
     GetMaterialApp(
       title: "Application",
